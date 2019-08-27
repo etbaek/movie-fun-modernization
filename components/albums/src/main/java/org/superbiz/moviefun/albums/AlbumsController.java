5dee686ccf6e07fd32d17f5255f895becddcb637
sjlee5 <sjlee5@lsis.com>
2019-08-27 10:41:24 +0900

    microservices

--- components/albums/src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
+++ components/albums/src/main/java/org/superbiz/moviefun/albums/AlbumsController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/album")
+@RequestMapping("/albums")
 public class AlbumsController {
 
     private AlbumsRepository albumsRepository;
